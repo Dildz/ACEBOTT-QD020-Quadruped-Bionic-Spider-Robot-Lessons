7be5956d14e16b9342c8c3ef8cca3b3979dbb837
Dildz <49305864+Dildz@users.noreply.github.com>
2025-09-17 10:02:19 +0200

    add servo pin reference, update variable names

--- Lesson 1/1.3_zero_0/src/main.cpp
+++ Lesson 1/1.3_zero_0/src/main.cpp
@@ -1,5 +1,15 @@
 /*
  * This function is used to zero the robot's leg servos to specific angles during the assembly process.
+ * 
+ * SERVO PIN REFERENCE:
+ * - PIN D0  = GPIO16
+ * - PIN D1  = GPIO5
+ * - PIN D2  = GPIO4
+ * - PIN D4  = GPIO2
+ * - PIN D5  = GPIO14
+ * - PIN D6  = GPIO12
+ * - PIN D7  = GPIO13
+ * - PIN D8  = GPIO15
  *
  * NOTES:
  * - The MG90S servo motors have a range of 0° to 180° - the range of motion must be declared in the attach() function as microsecond values to get the full range.
@@ -15,40 +25,40 @@
 #include <Servo.h>
 
 // GLOBAL VARIABLES
-Servo servo14_URP;    // upper right paw
-Servo servo12_URA;    // upper right arm
-Servo servo13_LRA;    // lower right arm
-Servo servo15_LRP;    // lower right paw
-Servo servo16_ULP;    // upper left paw
-Servo servo5_ULA;     // upper left arm
-Servo servo4_LLA;     // lower left arm
-Servo servo2_LLP;     // lower left paw
+Servo servoD5_URP;    // upper right paw
+Servo servoD6_URA;    // upper right arm
+Servo servoD7_LRA;    // lower right arm
+Servo servoD8_LRP;    // lower right paw
+Servo servoD0_ULP;    // upper left paw
+Servo servoD1_ULA;    // upper left arm
+Servo servoD2_LLA;    // lower left arm
+Servo servoD4_LLP;    // lower left paw
 
 
 // HELPER FUNCTION
 void zero() {   // Zeroing function for the main loop (calibrated values)
-  servo14_URP.write(140);   //   0° home | 180° extended | usable range =   0° to 180° | 140° level (zero)
-  servo12_URA.write(92);    // 180° home |  40° extended | usable range = 180° to  40° |  92° level (zero)
-  servo13_LRA.write(90);    //   0° home | 135° extended | usable range =   0° to 135° |  90° level (zero)
-  servo15_LRP.write(28);    // 175° home |   0° extended | usable range = 175° to   0° |  28° level (zero)
-  servo16_ULP.write(40);    // 177° home |   0° extended | usable range = 177° to   0° |  40° level (zero)
-  servo5_ULA.write(85);     //   0° home | 135° extended | usable range =   0° to 135° |  85° level (zero)
-  servo4_LLA.write(80);     // 175° home |  35° extended | usable range = 175° to  35° |  80° level (zero)
-  servo2_LLP.write(142);    //   2° home | 180° extended | usable range =   2° to 180° | 142° level (zero)
+  servoD5_URP.write(140);   //   0° home | 180° extended | usable range =   0° to 180° | 140° level (zero)
+  servoD6_URA.write(92);    // 180° home |  40° extended | usable range = 180° to  40° |  92° level (zero)
+  servoD7_LRA.write(90);    //   0° home | 135° extended | usable range =   0° to 135° |  90° level (zero)
+  servoD8_LRP.write(28);    // 175° home |   0° extended | usable range = 175° to   0° |  28° level (zero)
+  servoD0_ULP.write(40);    // 177° home |   0° extended | usable range = 177° to   0° |  40° level (zero)
+  servoD1_ULA.write(85);    //   0° home | 135° extended | usable range =   0° to 135° |  85° level (zero)
+  servoD2_LLA.write(80);    // 175° home |  35° extended | usable range = 175° to  35° |  80° level (zero)
+  servoD4_LLP.write(142);   //   2° home | 180° extended | usable range =   2° to 180° | 142° level (zero)
 }
 
 
 // SETUP
 void setup(){
   // Initialize the GPIO pin numbers & the range of motion of each servo (500 = 0°, 2500 = 180°)
-  servo14_URP.attach(14, 500, 2500);
-  servo12_URA.attach(12, 500, 2500);
-  servo13_LRA.attach(13, 500, 2500);
-  servo15_LRP.attach(15, 500, 2500);
-  servo16_ULP.attach(16, 500, 2500);
-  servo5_ULA.attach(5, 500, 2500);
-  servo4_LLA.attach(4, 500, 2500);
-  servo2_LLP.attach(2, 500, 2500);
+  servoD5_URP.attach(14, 500, 2500);
+  servoD6_URA.attach(12, 500, 2500);
+  servoD7_LRA.attach(13, 500, 2500);
+  servoD8_LRP.attach(15, 500, 2500);
+  servoD0_ULP.attach(16, 500, 2500);
+  servoD1_ULA.attach(5, 500, 2500);
+  servoD2_LLA.attach(4, 500, 2500);
+  servoD4_LLP.attach(2, 500, 2500);
 
   Serial.println("\nSetup completed\n");
 }
